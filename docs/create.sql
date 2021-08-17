c19c58ad7bec3ba66bb55e6febd1d91ee0eab9cb
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2021-08-16 22:02:09 -0300

    :hammer: Atualização do  texo

--- docs/create.sql
+++ docs/create.sql
@@ -11,88 +11,112 @@ drop table if exists Retirada_do_Objeto cascade;
 drop table if exists Objeto_Retirada cascade;
 
 CREATE TABLE Telefone(
-  id INT PRIMARY KEY,
-  numero VARCHAR(15) NOT NULL
+	id INT PRIMARY KEY,
+	numero VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE Usuario(
-  id INT PRIMARY KEY,
-  nome VARCHAR(50) NOT NULL,
-  email VARCHAR(30) NOT NULL,
-  cod_telefone INT NOT NULL,
-  CONSTRAINT fk_telefone FOREIGN KEY (cod_telefone) REFERENCES Telefone(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  tipo VARCHAR(15)
+	id INT PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	email VARCHAR(30) NOT NULL,
+	tipo VARCHAR(15),
+	cod_telefone INT NOT NULL,
+	CONSTRAINT fk_telefone FOREIGN KEY (cod_telefone) 
+		REFERENCES Telefone(id) 
+		ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE Deposito(
-  id INT PRIMARY KEY,
-  nome VARCHAR(50) NOT NULL,
-  quantidade INT NOT NULL
+	id INT PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	quantidade INT NOT NULL
 );
 
 CREATE TABLE Funcionario(
-  username VARCHAR(20) NOT NULL,
-  nome VARCHAR(50) NOT NULL,
-  email VARCHAR(30) NOT NULL,
-  id INT PRIMARY KEY,
-  cod_deposito INT NOT NULL,
-  CONSTRAINT fk_deposito FOREIGN KEY(cod_deposito) REFERENCES Deposito(id) ON DELETE RESTRICT ON UPDATE CASCADE
+	username VARCHAR(20) NOT NULL,
+	nome VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	id INT PRIMARY KEY,
+	cod_deposito INT NOT NULL,
+	CONSTRAINT fk_deposito FOREIGN KEY(cod_deposito) 
+		REFERENCES Deposito(id) 
+		ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE Mensagem(
-  id INT PRIMARY KEY,
-  conteudo VARCHAR(50)  NOT NULL,
-  id_objeto INT NOT NULL,
-  id_osuario INT NOT NULL,
-  cod_funcionario INT NOT NULL,
-  cod_usuario INT NOT NULL,
-  CONSTRAINT fk_funcionario FOREIGN KEY (cod_funcionario) REFERENCES Funcionario(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_usuario FOREIGN KEY (cod_usuario) REFERENCES Usuario(id) ON DELETE RESTRICT ON UPDATE CASCADE
+	id INT PRIMARY KEY,
+	conteudo VARCHAR(255)  NOT NULL,
+	id_objeto INT NOT NULL,
+	id_osuario INT NOT NULL,
+	cod_funcionario INT NOT NULL,
+	cod_usuario INT NOT NULL,
+	CONSTRAINT fk_funcionario FOREIGN KEY (cod_funcionario) 
+		REFERENCES Funcionario(id) 
+		ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT fk_usuario FOREIGN KEY (cod_usuario) 
+		REFERENCES Usuario(id) 
+		ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE Categoria(
-  id INT PRIMARY KEY,
-  nome VARCHAR(50) NOT NULL
+	id INT PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Objeto(
-  id INT PRIMARY KEY,
-  nome VARCHAR(50) NOT NULL,
-  id_usuario INT NOT NULL,
-  data_hora_entrada DATETIME NOT NULL,
-  descricao VARCHAR(50) NOT NULL,
-  situacao VARCHAR(30) NOT NULL,
-  status VARCHAR(15) NOT NULL,
-  cod_categoria INT NOT NULL,
-  CONSTRAINT fk_categoria FOREIGN KEY(cod_categoria) REFERENCES Categoria(id) ON DELETE RESTRICT ON UPDATE CASCADE
+	id INT PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	id_usuario INT NOT NULL,
+	data_hora_entrada DATETIME NOT NULL,
+	descricao VARCHAR(50) NOT NULL,
+	situacao VARCHAR(30) NOT NULL,
+	status VARCHAR(15) NOT NULL,
+	cod_categoria INT NOT NULL,
+	CONSTRAINT fk_categoria FOREIGN KEY(cod_categoria) 
+		REFERENCES Categoria(id) 
+		ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE Deposito_Objeto(
 	cod_deposito INT NOT NULL,
-  cod_objeto INT NOT NULL,
-	CONSTRAINT deposito_fk FOREIGN KEY(cod_deposito) REFERENCES Deposito(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT objeto_fk FOREIGN KEY(cod_objeto) REFERENCES Objeto(id) ON DELETE RESTRICT ON UPDATE CASCADE
+	cod_objeto INT NOT NULL,
+	CONSTRAINT deposito_fk FOREIGN KEY(cod_deposito) 
+		REFERENCES Deposito(id) 
+		ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT objeto_fk FOREIGN KEY(cod_objeto) 
+		REFERENCES Objeto(id) 
+		ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE Objeto_Usuario(
 	cod_objeto INT NOT NULL,
 	cod_usuario INT NOT NULL,
-  CONSTRAINT fk_objeto2 FOREIGN KEY(cod_objeto) REFERENCES Objeto(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_usuario2 FOREIGN KEY(cod_usuario) REFERENCES Usuario(id) ON DELETE RESTRICT ON UPDATE CASCADE
+	CONSTRAINT fk_objeto2 FOREIGN KEY(cod_objeto) 
+		REFERENCES Objeto(id) 
+		ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT fk_usuario2 FOREIGN KEY(cod_usuario) 
+		REFERENCES Usuario(id) 
+		ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE Retirada_do_Objeto(
-  id INT PRIMARY KEY,
-  cod_objeto INT NOT NULL,
-  id_achado_por INT NOT NULL,
-  id_retirado_por INT NOT NULL,
-  data_hora_resgate DATETIME NOT NULL,
-  CONSTRAINT fk_retirada FOREIGN KEY(cod_objeto) REFERENCES Objeto(id) ON DELETE RESTRICT ON UPDATE CASCADE
+	id INT PRIMARY KEY,
+	cod_objeto INT NOT NULL,
+	id_achado_por INT NOT NULL,
+	id_retirado_por INT NOT NULL,
+	data_hora_resgate DATETIME NOT NULL,
+	CONSTRAINT fk_retirada FOREIGN KEY(cod_objeto) 
+		REFERENCES Objeto(id) 
+		ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE Objeto_Retirada(
-  cod_objeto INT NOT NULL,
-  cod_retirada INT NOT NULL,
-  CONSTRAINT fk_objeto3 FOREIGN KEY(cod_objeto) REFERENCES Objeto(id)  ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_retirada3 FOREIGN KEY(cod_retirada) REFERENCES Retirada_do_Objeto(id)  ON DELETE RESTRICT ON UPDATE CASCADE
+	cod_objeto INT NOT NULL,
+	cod_retirada INT NOT NULL,
+	CONSTRAINT fk_objeto3 FOREIGN KEY(cod_objeto) 
+		REFERENCES Objeto(id) 
+		ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT fk_retirada3 FOREIGN KEY(cod_retirada) 
+		REFERENCES Retirada_do_Objeto(id) 
+		ON DELETE RESTRICT ON UPDATE CASCADE
 );
