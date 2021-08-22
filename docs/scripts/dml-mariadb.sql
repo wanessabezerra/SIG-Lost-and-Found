bbdafe0ec8b21dceff8e1753da89ec9128c55506
renatak12 <54039830+renatak12@users.noreply.github.com>
2021-08-21 23:40:31 -0300

    :hammer: corrigindo consultas #21

--- docs/scripts/dml-mariadb.sql
+++ docs/scripts/dml-mariadb.sql
@@ -58,7 +58,7 @@ FROM Objeto AS o
 INNER JOIN Usuario AS u
 INNER JOIN Mensagem AS m
 ON o.status = 'encontrado'
-WHERE u.id = o.id AND m.cod_usuario = u.id;
+WHERE o.id_usuario = u.id AND m.id_objeto = o.id;
 
 # 10. Encontre os nomes de todos os usuários no banco de dados, que não são tipo discente.
 
